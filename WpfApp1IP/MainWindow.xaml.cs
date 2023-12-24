83430540a1fae2ac2118c9d946303ed3d846f66e
Tomasz Fryckowski <tomaszfryckowski@gmail.com>
2023-12-24 11:49:38 +0100

    Add project files.

---
+++ WpfApp1IP/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1IP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+  
+                ProcessStartInfo psi = new ProcessStartInfo("cmd", "/c route print")
+                {
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+
+                Process p = Process.Start(psi);
+                string output = p.StandardOutput.ReadToEnd();
+                p.WaitForExit();
+
+                MessageBox.Show(output);
+            
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo("cmd", "/c route print")
+            {
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            Process p = Process.Start(psi);
+            string output = p.StandardOutput.ReadToEnd();
+            string error = p.StandardError.ReadToEnd();
+            p.WaitForExit();
+
+            if (!string.IsNullOrEmpty(error))
+            {
+                MessageBox.Show("Wystąpił błąd: " + error);
+            }
+            else
+            {
+                // Zapisz wyniki do pliku
+                File.WriteAllText("routingTable.txt", output);
+            }
+        }
+    }
+    
+}
