ee80e18bac3cf766d3ab317c1625d40aab37499f
Tomasz Fryckowski <tomaszfryckowski@gmail.com>
2023-12-28 16:21:26 +0100

    Dodanie buttonow , oraz wyswietlanie twojego ip

--- WpfApp1IP/MainWindow.xaml.cs
+++ WpfApp1IP/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System.Diagnostics;
 using System.IO;
+using System.Net.Sockets;
+using System.Net;
+using System.Reflection.Emit;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,9 +24,11 @@ namespace WpfApp1IP
         public MainWindow()
         {
             InitializeComponent();
+
+            DisplayIPAddress();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        public void Button_Click(object sender, RoutedEventArgs e)
         {
             
   
@@ -43,7 +48,7 @@ namespace WpfApp1IP
 
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        public void Button_Click_1(object sender, RoutedEventArgs e)
         {
             ProcessStartInfo psi = new ProcessStartInfo("cmd", "/c route print")
             {
@@ -56,6 +61,9 @@ namespace WpfApp1IP
             Process p = Process.Start(psi);
             string output = p.StandardOutput.ReadToEnd();
             string error = p.StandardError.ReadToEnd();
+            string path = @"C:\Users\tfryckowski\Desktop\testMyTest.txt";
+           
+            
             p.WaitForExit();
 
             if (!string.IsNullOrEmpty(error))
@@ -64,10 +72,73 @@ namespace WpfApp1IP
             }
             else
             {
-                // Zapisz wyniki do pliku
-                File.WriteAllText("routingTable.txt", output);
+                
+                File.WriteAllText(path, output);
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            
+                ProcessStartInfo psi = new ProcessStartInfo("cmd", "/c route print")
+                {
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+
+                Process p = Process.Start(psi);
+                string output = p.StandardOutput.ReadToEnd();
+                string error = p.StandardError.ReadToEnd();
+                string path = @"C:\Users\tfryckowski\Desktop\testMyTest.txt";
+            p.WaitForExit();
+
+                if (!string.IsNullOrEmpty(error))
+                {
+                    MessageBox.Show("Wystąpił błąd: " + error);
+                }
+                else
+                {
+                    // Zapisz wyniki do pliku
+                    File.WriteAllText(path, output);
+                }
+            
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo("cmd", "/c route print")
+            {
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            Process p = Process.Start(psi);
+            string output = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+
+            MessageBox.Show(output);
+        }
+
+
+
+        public void DisplayIPAddress()
+        {
+            string hostName = Dns.GetHostName();
+            IPAddress[] ipAddresses = Dns.GetHostAddresses(hostName);
+            foreach (IPAddress ipAddress in ipAddresses)
+            {
+                if (ipAddress.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    ipLabel.Content = "Twój adress ip:"+" "+ipAddress.ToString();
+                   
+                    break;
+                }
             }
         }
     }
-    
+
 }
+    
